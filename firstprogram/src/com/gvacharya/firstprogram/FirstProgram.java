dae4a703b92b12b56e3eb4129a443021f905c0a6
Lenovo <Lenovo@DESKTOP-P50M9N9>
2024-06-21 13:55:15 +0530

    push

---
+++ firstprogram/src/com/gvacharya/firstprogram/FirstProgram.java
@@ -0,0 +1,7 @@
+package com.gvacharya.firstprogram;
+
+public class FirstProgram {
+ public static void main(String [] args) {
+ System.out.println("First Program");
+}
+}
---
+++ firstprogram/src/com/gvacharya/mainclass_structure/MainClass_Structure.java
@@ -0,0 +1,15 @@
+package com.gvacharya.mainclass_structure;
+
+import com.gvacharya.class_structure.ClassStructure;
+
+public class MainClass_Structure {
+	public static void main(String [] args) {
+		ClassStructure obj = new ClassStructure();
+        System.out.println(obj.getPrice());
+        System.out.println(obj.getBarcode());
+        System.out.println(obj.getBrandname());
+// CTRL + shift + O is shortcut for importing		
+
+	}
+
+}
